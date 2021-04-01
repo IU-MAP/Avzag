45e4a1abbd0e4ae8d938d9a702c92e95cf27d9eb
alkaitagi <alkaitagi@outlook.com>
2021-04-01 20:02:43 +0300

    Fix base route

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: "/Avzag/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Avzag/" : "/",
   productionSourceMap: false,
   configureWebpack: {
     devtool: "source-map",
