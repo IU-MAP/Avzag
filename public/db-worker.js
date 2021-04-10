0c6df7e72e0ebc41f83487129092cd3600d0420f
alkaitagi <alkaitagi@outlook.com>
2021-04-10 23:59:37 +0300

    Fix service worker

--- public/db-worker.js
+++ public/db-worker.js
@@ -1,33 +1,45 @@
-import { openDB } from "idb";
-
-let db;
 let root;
-let lects;
+let db;
 
 async function fillDB(dictionaries) {
   function fillLect(lect, dictionary) {
     console.log("loading", lect);
     const st = tx.objectStore(lect);
-    return dictionary.map((d) => st.put(d /* , d.forms[0].text.plain */));
+    return dictionary.map((d) => st.add(d /* , d.forms[0].text.plain */));
   }
 
   const lects = Object.keys(dictionaries);
+  console.log("lects", lects);
   const tx = db.transaction(lects, "readwrite");
   await Promise.all(lects.flatMap((l) => fillLect(l, dictionaries[l])));
 }
 
-async function load() {
-  db = await openDB("avzag", 1);
-  const dictionaries = await loadLectsJSON("dictionary");
-  await fillDB(dictionaries);
-  postMessage(Object.keys(dictionaries));
+async function load(lects) {
+  const dictionaries = await loadLectsJSON("dictionary", lects);
+  lects = Object.keys(dictionaries);
+
+  const req = indexedDB.open("avzag", 1);
+
+  req.onupgradeneeded = (e) => {
+    const db = e.target.result;
+    lects.forEach((l) => {
+      if (db.objectStoreNames.contains(l)) db.deleteObjectStore(l);
+      db.createObjectStore(l, { autoIncrement: true });
+    });
+  };
+
+  req.onsuccess = async (e) => {
+    db = e.target.result;
+    await fillDB(dictionaries);
+    postMessage(lects);
+  };
 }
 
 onmessage = (e) => {
+  console.log("received msg");
   const args = e.data.split("+");
   root = args[0];
-  lects = args.split(".");
-  load();
+  load(args[1].split("."));
 };
 
 async function loadJSON(filename, defaultValue) {
@@ -35,7 +47,7 @@ async function loadJSON(filename, defaultValue) {
     .then((r) => r.json())
     .catch(() => defaultValue);
 }
-async function loadLectsJSON(filename) {
+async function loadLectsJSON(filename, lects) {
   const files = {};
   for (const lect of lects) {
     const file = await loadJSON(lect + "/" + filename);
--- src/Dictionary/main.ts
+++ src/Dictionary/main.ts
@@ -11,32 +11,17 @@ export const lects_ = shallowRef([] as string[]);
 
 let t = 0;
 const worker = new Worker("db-worker.js");
-// worker.onmessage = function (e) {
-//   console.log("Db loaded in", (Date.now() - t) / 1000);
-//   processing.loading = false;
-//   lects_.value = e.data;
-// };
-
-async function cleanDB() {
-  await deleteDB("avzag");
-  db = await openDB("avzag", 1, {
-    upgrade(db) {
-      lects_.value.forEach((l) => {
-        if (db.objectStoreNames.contains(l)) db.deleteObjectStore(l);
-        db.createObjectStore(l, { autoIncrement: true });
-      });
-    },
-  });
-}
+worker.onmessage = function (e) {
+  console.log("Db loaded in", (Date.now() - t) / 1000);
+  processing.loading = false;
+  lects_.value = e.data;
+};
 
 watch(lects, async () => {
   processing.loading = true;
   console.log("Loading DB...");
   t = Date.now();
 
-  // fetching json
   dictionaryMeta.value = await loadJSON("dictionary");
-
-  await cleanDB();
   worker.postMessage(root + "+" + lects.value.join("."));
 });
