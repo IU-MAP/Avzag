2e7398c5ef31ffaa36f9b1dfdb6c8b210284409f
alkaitagi <alkaitagi@outlook.com>
2021-04-11 10:56:58 +0300

    Fixes

--- src/Dictionary/search.ts
+++ src/Dictionary/search.ts
@@ -1,49 +1,52 @@
+/* eslint-disable no-unmodified-loop-condition */
 import { db, lects_, processing } from "./main";
 import { Entry, Search } from "./types";
 
 async function queryDictionaries(query: string[], queryMode: string) {
-  const ss_ = ss;
-  function fits(e: Entry) {
-    const area = queryMode === "Tags" ? e.tags ?? "" : e.translation;
+  function fits(entry: Entry) {
+    const area = queryMode === "Tags" ? entry.tags ?? "" : entry.translation;
     return query.some((q) => area?.includes(q));
   }
+  function push(entry: Entry, lect: string) {
+    const t = entry.translation;
+    if (!search[t]) search[t] = {};
+    if (!search[t][lect]) search[t][lect] = [];
+    search[t][lect].push(entry as Entry);
+  }
 
+  const ss_ = ss;
   const search = {} as Search;
   const tx = db.transaction(lects_.value);
-  await Promise.all(
-    lects_.value.map(async (l) => {
-      let cr = await tx.objectStore(l).openCursor();
-      while (cr) {
-        if (ss !== ss_) return {};
-        const entry = cr.value as Entry;
-        if (fits(entry)) {
-          const ts = entry.translation;
-          if (!search[ts]) search[ts] = {};
-          if (!search[ts][l]) search[ts][l] = [];
-          search[ts][l].push(cr.value as Entry);
-        }
-        cr = await cr.continue();
-      }
-    })
-  );
+
+  for (const l of lects_.value) {
+    console.log("looking in", l, "with", ss === ss_);
+    let cr = await tx.objectStore(l).openCursor();
+    while (cr && ss === ss_) {
+      console.log("looking in", l);
+      const entry = cr.value as Entry;
+      if (fits(entry)) push(entry, l);
+      cr = await cr.continue();
+    }
+  }
   return search;
 }
 
 async function findTranslations(lect: string, query: string[]) {
   const ss_ = ss;
   const translations = new Set<string>();
+
   let cr = await db.transaction(lect).store.openCursor();
-  while (cr) {
-    if (ss !== ss_) return [];
+  while (cr && ss === ss_) {
     const { forms, translation } = cr.value as Entry;
     if (forms.some(({ text }) => query.some((q) => text.plain.includes(q))))
       translations.add(translation);
     cr = await cr.continue();
   }
+
   return [...translations];
 }
 
-let ss = Symbol("srch");
+const ss = Symbol("srch");
 
 export async function search(
   lect: string,
@@ -51,13 +54,18 @@ export async function search(
   queryMode = "Translation"
 ) {
   if (!db) return {};
-  ss = Symbol("srch");
+  // ss = Symbol("srch");
 
   processing.searching = true;
 
+  console.log("Started search");
   const results = !lect
-    ? queryDictionaries(query, queryMode)
-    : queryDictionaries(await findTranslations(lect, query), "Translation");
+    ? await queryDictionaries(query, queryMode)
+    : await queryDictionaries(
+        await findTranslations(lect, query),
+        "Translation"
+      );
+  console.log("Found", results);
 
   processing.searching = false;
   return results;
