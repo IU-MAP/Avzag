efa636508f34adad85713e4f5cbde3f37515d026
alkaitagi <alkaitagi@outlook.com>
2021-04-12 20:36:10 +0300

    Add comments

--- src/Dictionary/search.ts
+++ src/Dictionary/search.ts
@@ -3,10 +3,13 @@ import { Entry, Search } from "./types";
 
 async function queryDictionaries(query: string[], queryMode: string) {
   function fits(entry: Entry) {
+    // check if the word fits in the query.
     const area = queryMode === "Tags" ? entry.tags ?? "" : entry.translation;
     return query.some((q) => area?.includes(q));
   }
+
   function push(entry: Entry, lect: string) {
+    // add the word to the result under its translation.
     const t = entry.translation;
     if (!search[t]) search[t] = {};
     if (!search[t][lect]) search[t][lect] = [];
@@ -17,10 +20,9 @@ async function queryDictionaries(query: string[], queryMode: string) {
   const tx = db.transaction(lects_.value);
 
   for (const l of lects_.value) {
-    console.log("looking in", l, "with");
+    console.log("Seaching in", l);
     let cr = await tx.objectStore(l).openCursor();
     while (cr) {
-      console.log("looking in", l);
       const entry = cr.value as Entry;
       if (fits(entry)) push(entry, l);
       cr = await cr.continue();
@@ -30,6 +32,7 @@ async function queryDictionaries(query: string[], queryMode: string) {
 }
 
 async function findTranslations(lect: string, query: string[]) {
+  // look through all forms in the language and collect their translations.
   const translations = new Set<string>();
 
   let cr = await db.transaction(lect).store.openCursor();
@@ -48,13 +51,14 @@ export async function search(
   query: string[],
   queryMode = "Translation"
 ) {
-  // if (dbInfo.state !== "ready") return {};
-  // const results = !lect
-  //   ? await queryDictionaries(query, queryMode)
-  //   : await queryDictionaries(
-  //       await findTranslations(lect, query),
-  //       "Translation"
-  //     );
-  // return results;
-  return {};
+  if (dbInfo.state !== "ready") return {};
+  // if no target language, then user gave meaning (translation);
+  // otherwise we need to get translation from the form first.
+  const results = !lect
+    ? await queryDictionaries(query, queryMode)
+    : await queryDictionaries(
+        await findTranslations(lect, query),
+        "Translation"
+      );
+  return results;
 }
