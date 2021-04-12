e2c1b820264328ad37cd391a61e1096d8d36df44
alkaitagi <alkaitagi@outlook.com>
2021-04-12 19:25:05 +0300

    Clean up search

--- src/Dictionary/search.ts
+++ src/Dictionary/search.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-unmodified-loop-condition */
 import { db, lects_, processing } from "./main";
 import { Entry, Search } from "./types";
 
@@ -14,14 +13,13 @@ async function queryDictionaries(query: string[], queryMode: string) {
     search[t][lect].push(entry as Entry);
   }
 
-  const ss_ = ss;
   const search = {} as Search;
   const tx = db.transaction(lects_.value);
 
   for (const l of lects_.value) {
-    console.log("looking in", l, "with", ss === ss_);
+    console.log("looking in", l, "with");
     let cr = await tx.objectStore(l).openCursor();
-    while (cr && ss === ss_) {
+    while (cr) {
       console.log("looking in", l);
       const entry = cr.value as Entry;
       if (fits(entry)) push(entry, l);
@@ -32,11 +30,10 @@ async function queryDictionaries(query: string[], queryMode: string) {
 }
 
 async function findTranslations(lect: string, query: string[]) {
-  const ss_ = ss;
   const translations = new Set<string>();
 
   let cr = await db.transaction(lect).store.openCursor();
-  while (cr && ss === ss_) {
+  while (cr) {
     const { forms, translation } = cr.value as Entry;
     if (forms.some(({ text }) => query.some((q) => text.plain.includes(q))))
       translations.add(translation);
@@ -46,28 +43,24 @@ async function findTranslations(lect: string, query: string[]) {
   return [...translations];
 }
 
-const ss = Symbol("srch");
-
 export async function search(
   lect: string,
   query: string[],
   queryMode = "Translation"
 ) {
-  // if (processing.dbState.state !== "ready") return {};
-  // // ss = Symbol("srch");
+  if (processing.dbState.state !== "ready") return {};
 
-  // processing.searching = true;
+  processing.searching = true;
 
-  // console.log("Started search");
-  // const results = !lect
-  //   ? await queryDictionaries(query, queryMode)
-  //   : await queryDictionaries(
-  //       await findTranslations(lect, query),
-  //       "Translation"
-  //     );
-  // console.log("Found", results);
+  console.log("Started search");
+  const results = !lect
+    ? await queryDictionaries(query, queryMode)
+    : await queryDictionaries(
+        await findTranslations(lect, query),
+        "Translation"
+      );
+  console.log("Found", results);
 
-  // processing.searching = false;
-  // return results;
-  return {} as Search;
+  processing.searching = false;
+  return results;
 }
