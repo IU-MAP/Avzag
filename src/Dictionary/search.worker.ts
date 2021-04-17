6731df1dae367aa053a509ec2f2a65700c3277cd
alkaitagi <alkaitagi@outlook.com>
2021-04-18 00:44:22 +0300

    Fix communication

--- src/Dictionary/search.worker.ts
+++ src/Dictionary/search.worker.ts
@@ -1,35 +1,30 @@
-import { Entry, Search, SearchWorkerCommand } from "./types";
+import { Entry, SearchWorkerCommand } from "./types";
 import { IDBPDatabase, openDB } from "idb";
 
 let db: IDBPDatabase;
 let lects: string[];
 
-async function queryDictionaries(query: string[], queryMode: string) {
+async function queryDictionaries(query: string[], queryMode = "Translation") {
   function fits(entry: Entry) {
     // check if the word fits in the query.
     const area = queryMode === "Tags" ? entry.tags ?? "" : entry.translation;
     return query.some((q) => area?.includes(q));
   }
-
   async function search(lect: string) {
     let cr = await db.transaction(lect).store.openCursor();
     while (cr) {
-      console.log("Seaching in", lect);
       const entry = cr.value as Entry;
-      if (fits(entry)) postMessage({ lect, entry });
+      if (fits(entry)) postMessage(JSON.stringify({ lect, entry }));
       cr = await cr.continue();
     }
+    console.log("Finished", lect);
   }
-
-  const results = {} as Search;
   await Promise.all(lects.map((l) => search(l)));
-  return results;
 }
 
 async function findTranslations(lect: string, query: string[]) {
   // look through all forms in the language and collect their translations.
   const translations = new Set<string>();
-
   let cr = await db.transaction(lect).store.openCursor();
   while (cr) {
     const { forms, translation } = cr.value as Entry;
@@ -37,24 +32,7 @@ async function findTranslations(lect: string, query: string[]) {
       translations.add(translation);
     cr = await cr.continue();
   }
-
   return [...translations];
-}
-
-export async function search(
-  lect: string,
-  query: string[],
-  queryMode = "Translation"
-) {
-  // if no target language, then user gave meaning (translation);
-  // otherwise we need to get translation from the form first.
-  const results = !lect
-    ? await queryDictionaries(query, queryMode)
-    : await queryDictionaries(
-        await findTranslations(lect, query),
-        "Translation"
-      );
-  return results;
 }
 
 onmessage = async (e) => {
@@ -66,6 +44,8 @@ onmessage = async (e) => {
     return;
   }
   if (!lects) return;
-  const searchResult = await search(data.lect, data.query, data.queryMode);
-  postMessage(JSON.stringify(searchResult));
+
+  const { lect, query, queryMode } = { ...data };
+  if (lect) queryDictionaries(await findTranslations(lect, query));
+  else queryDictionaries(query, queryMode);
 };
