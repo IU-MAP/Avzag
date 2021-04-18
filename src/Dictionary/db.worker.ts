b1bd4b8af49f5006edc13549f72be61e498a65d3
qazybi <k.askarbek@innopolis.university>
2021-04-18 21:48:08 +0300

    needed db is not deleted instead just updated

--- src/Dictionary/db.worker.ts
+++ src/Dictionary/db.worker.ts
@@ -1,23 +1,34 @@
-import { openDB, IDBPDatabase } from "idb";
+import { openDB, deleteDB, IDBPDatabase } from "idb";
 import { loadLectsJSON } from "@/store";
 import { Entry, DBWorkerState } from "./types";
 
 let db: IDBPDatabase;
 
 async function cleanDB(lects: string[]) {
+  // await deleteDB("avzag");
   db = await openDB("avzag", 1, {
     upgrade(db) {
+      console.log("updating");
       for (const l of lects) {
         if (pending) return;
-        if (db.objectStoreNames.contains(l)) db.deleteObjectStore(l);
-        db.createObjectStore(l, { autoIncrement: true });
+        // if table for the language does not exist then create one
+        if (!db.objectStoreNames.contains(l)) {
+          db.createObjectStore(l, { autoIncrement: true });
+        }
+      }
+      // delete unnecessary tables from DB
+      for (const tableName of db.objectStoreNames) {
+        if (!lects.includes(tableName)) {
+          db.deleteObjectStore(tableName);
+        }
       }
     },
   });
-  for (const l of lects) {
-    if (pending) return;
-    await db.clear(l);
-  }
+
+  // for (const l of lects) {
+  //   if (pending) return;
+  //   await db.clear(l);
+  // }
 }
 
 async function fillDB(dictionaries: Record<string, Entry[]>) {
@@ -25,21 +36,94 @@ async function fillDB(dictionaries: Record<string, Entry[]>) {
   const size = Object.values(dictionaries).reduce((s, d) => s + d.length, 0);
   const step = 1024;
   let done = 0;
-
-  for (const [l, ds] of Object.entries(dictionaries)) {
-    const st = db.transaction(l, "readwrite").store;
-    const current = ds.length;
+  // let counter = 0;
+  for (const tableName in dictionaries) {
+    // if table exist then check whether should we update it
+    if (!db.objectStoreNames.contains(tableName)) {
+      continue;
+    }
+    // get the objectStore
+    const langDict = await db.transaction(tableName, "readwrite").store;
     const puts = [];
-    for (const d of ds) {
-      if (pending) return;
-      puts.push(st.add(d));
-      if (!(puts.length % step)) {
-        done += step;
-        const progress = Math.round((done / size) * 100);
-        postState(
-          "loading",
-          `[${progress}%] Loading ${l} - ${puts.length} of ${current}`
-        );
+    // let flag = true;
+    const tableSize = await langDict.count();
+    // if table is empty
+    if (tableSize === 0) {
+      for (const element of dictionaries[tableName]) {
+        puts.push(langDict.add(element));
+
+        // update state
+        if (!(puts.length % step)) {
+          done += step;
+          const progress = Math.round((done / size) * 100);
+          postState(
+            "loading",
+            `[${progress}%] Loading ${tableName} - ${puts.length} of ${dictionaries[tableName].length}`
+          );
+        }
+      }
+    } else {
+      // table is not empty thus update it with new values
+      const allValues = await langDict.getAll();
+
+      const wordsToAdd: Entry[] = [];
+      const wordsToDelete: Entry[] = [];
+
+      let i = 0;
+      let j = 0;
+
+      while (i < allValues.length && j < dictionaries[tableName].length) {
+        if (
+          JSON.stringify(allValues[i]) !==
+          JSON.stringify(dictionaries[tableName][j])
+        ) {
+          if (wordsToAdd.includes(allValues[i])) {
+            delete wordsToAdd[allValues[i]];
+          } else {
+            wordsToDelete.push(allValues[i]);
+          }
+
+          if (wordsToDelete.includes(dictionaries[tableName][j])) {
+            const index = wordsToDelete.indexOf(dictionaries[tableName][j], 0);
+            if (index > -1) {
+              wordsToDelete.splice(index, 1);
+            }
+          } else {
+            wordsToAdd.push(dictionaries[tableName][j]);
+          }
+        }
+        i++;
+        j++;
+      }
+
+      if (i !== allValues.length) {
+        // remove old words
+        while (i < allValues.length) {
+          langDict.delete(allValues[i]);
+          i++;
+        }
+        console.log("removing");
+      } else if (j !== dictionaries[tableName].length) {
+        // add new words
+        while (j < dictionaries[tableName].length) {
+          puts.push(langDict.add(dictionaries[tableName]));
+          j++;
+        }
+      }
+      console.log(wordsToAdd, wordsToDelete);
+
+      for (const word of wordsToAdd) {
+        for (let counter = 0; counter < wordsToAdd.length; counter++) {
+          puts.push(langDict.add(allValues[counter]));
+          i++;
+        }
+      }
+
+      for (const word of wordsToDelete) {
+        for (let counter = 0; counter < wordsToDelete.length; counter++) {
+          langDict.delete(allValues[counter]);
+          i++;
+        }
       }
     }
     await Promise.all(puts);
@@ -58,10 +142,10 @@ async function load(lects: string[]) {
   if (pending) return postState("fetching");
   const t0 = performance.now();
   await cleanDB(lects);
-  const t1 = performance.now();
-  console.log("Call to cleanDB " + (t1 - t0) + " milliseconds.");
   if (pending) return postState("fetching");
   await fillDB(dictionaries);
+  const t1 = performance.now();
+  console.log("Call to cleanDB " + (t1 - t0) + " milliseconds.");
   postState("ready");
 }
 
