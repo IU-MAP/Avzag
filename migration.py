4865f4077ac6355fd3008577f9d7299af389c279
alkaitagi <alkaitagi@outlook.com>
2021-04-21 18:29:00 +0300

    add migration

---
+++ migration.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+root = os.path.dirname(os.path.abspath(__file__))+'\\'
+lects = ["Iron", "Kaitag", "Keren", "Kabardian", "Kuirin"]
+
+for l in lects:
+    path = f"{root}{l}\\dictionary.json"
+    with open(path, encoding='utf-8') as f:
+        data = json.loads(f.read())
+
+    for d in data:
+        c = {"meaning": d["translation"]}
+        if "explanation" in d:
+            c["notes"] = [d["explanation"]]
+
+        d["concepts"] = [c]
+        del d["translation"]
+
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
