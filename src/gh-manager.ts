c7805479ce130a04e68c83d0b733828474091dc5
alkaitagi <alkaitagi@outlook.com>
2021-04-24 16:36:02 +0300

    Add pulls

--- src/gh-manager.ts
+++ src/gh-manager.ts
@@ -2,28 +2,20 @@ import { Octokit } from "@octokit/rest";
 
 const octokit = new Octokit({ auth: process.env.VUE_APP_GH_TOKEN });
 
-function getDate() {
-  const date = new Date();
-  const components = [
-    date.getUTCFullYear(),
-    date.getUTCMonth(),
-    date.getUTCDate(),
-  ];
-  return components.join(".");
-}
-
 async function createBranch() {
   const store = await octokit.repos.getBranch({
     owner: "IU-MAP",
     repo: "avzag",
     branch: "store",
   });
+  const branch = "store-editor-" + Date.now();
   await octokit.git.createRef({
     owner: "IU-MAP",
     repo: "avzag",
-    ref: "refs/heads/store-editor-" + getDate(),
+    ref: "refs/heads/" + branch,
     sha: store.data.commit.sha,
   });
+  return branch;
 }
 
 export async function pushToStore(
@@ -32,13 +24,20 @@ export async function pushToStore(
   message: string
 ) {
   content = btoa(unescape(encodeURIComponent(content)));
-  await createBranch();
+  const branch = await createBranch();
   await octokit.repos.createOrUpdateFileContents({
     owner: "IU-MAP",
     repo: "avzag",
     path,
     content,
     message,
-    branch: "store-test-GH-PR",
+    branch,
+  });
+  await octokit.pulls.create({
+    owner: "IU-MAP",
+    repo: "avzag",
+    title: message,
+    head: branch,
+    base: "store",
   });
 }
