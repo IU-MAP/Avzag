b45ad0f6c1dbaced970dd25c4fdbbc9109401427
alkaitagi <alkaitagi@outlook.com>
2021-04-30 00:25:21 +0300

    Get last update time

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { ref, toRaw, watch } from "vue";
+import { Octokit } from "@octokit/rest";
 
 export const lects = ref([] as string[]);
 export const root =
@@ -12,16 +13,34 @@ watch(
   { deep: true }
 );
 
-export async function loadJSON<T>(filename: string, defaultValue?: T) {
-  return await fetch(root + filename + ".json")
-    .then((r) => r.json())
+const octokit = new Octokit();
+export async function isOutdated(path: string) {
+  const commits = await octokit.repos.listCommits({
+    owner: "IU-MAP",
+    repo: "avzag",
+    sha: "store",
+    path: path,
+  });
+  const time = commits.data[0].commit.committer?.date;
+  if (!time) return;
+  const timestamp = new Date(time).getTime();
+  console.log(root + path, time);
+}
+
+export async function loadJSON<T>(path: string, defaultValue?: T) {
+  path += ".json";
+  const response = await fetch(root + path);
+  console.log(Array.from(response.headers.entries()));
+  isOutdated(path);
+  return await response
+    .json()
     .then((j) => j as T)
     .catch(() => defaultValue as T);
 }
-export async function loadLectsJSON<T>(filename: string, lects_?: string[]) {
+export async function loadLectsJSON<T>(path: string, lects_?: string[]) {
   const files = {} as Record<string, T>;
   for (const lect of lects_ ?? lects.value) {
-    const file = await loadJSON<T>(lect + "/" + filename);
+    const file = await loadJSON<T>(lect + "/" + path);
     if (file) files[lect] = file;
   }
   return files;
