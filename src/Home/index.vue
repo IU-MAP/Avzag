149aaa7239a1f850ed157bb170b219d03d50fe69
alkaitagi <alkaitagi@outlook.com>
2021-05-02 16:08:37 +0300

    Copy home file changes

--- src/Home/index.vue
+++ src/Home/index.vue
@@ -34,7 +34,7 @@
         <div v-if="about" id="about" class="col-1 card text-center small">
           <h1>Ævzag</h1>
           <div class="row-1 wrap center">
-            <router-link to="/editor/dictionary">
+            <router-link to="/editor/phonology">
               <btn icon="construction" text="Editor" />
             </router-link>
             <a href="https://t.me/avzag" class="wrap">
@@ -67,23 +67,39 @@ import InputQuery from "@/components/Query/InputQuery.vue";
 
 import {
   computed,
-  onMounted,
-  onUnmounted,
   ref,
-  watch,
   defineComponent,
+  toRaw,
+  onMounted,
+  watch,
+  onUnmounted,
 } from "vue";
 import { useRouter } from "vue-router";
-import { lastUpdated, lects } from "@/store";
-import { reset, catalogue, search, query } from "./main";
+import { cache, checkOutdated, lects, loadJSON, storage } from "@/store";
+import { catalogue, search, query } from "./main";
 import { createMap } from "./map";
 
 export default defineComponent({
   components: { Marker, Card, InputQuery },
   setup() {
-    reset();
     const router = useRouter();
     onMounted(() => createMap());
+    onUnmounted(async () => {
+      lects.value = [...search.selected];
+      await storage.setItem("lects", toRaw(lects.value));
+      await checkOutdated();
+    });
+    watch(
+      lects,
+      () => {
+        if (!search.selected.size && lects.value.length)
+          lects.value.forEach((l) => search.selected.add(l));
+      },
+      { immediate: true }
+    );
+
+    delete cache.records.value["catalogue.json"];
+    loadJSON("catalogue", []).then((j) => (catalogue.value = j));
 
     const empty = computed(() => !search.selected.size);
     const about = ref(false);
@@ -93,26 +109,12 @@ export default defineComponent({
       else search.selected.add(name);
     }
     function load() {
-      lects.value = [...search.selected];
-      lastUpdated.value.lects = Date.now();
       router.push(
         localStorage.urlUser
           ? { path: localStorage.urlUser }
-          : { name: "dictionary" }
+          : { name: "phonology" }
       );
     }
-
-    watch(
-      catalogue,
-      () =>
-        (search.selected = new Set(
-          JSON.parse(localStorage.lects ?? "[]") as string[]
-        )),
-      { immediate: true }
-    );
-    onUnmounted(
-      () => (localStorage.lects = JSON.stringify([...search.selected]))
-    );
 
     return { catalogue, query, search, empty, about, toggleLect, load };
   },
